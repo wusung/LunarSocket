01409217eb42a19510c98bbb395a2fa0b44f8c27
Richard Dorian <75615715+RichardDorian@users.noreply.github.com>
2022-06-06 17:44:59 +0000

    fix: :ambulance: fix unknown role crash

--- src/utils/roles.ts
+++ src/utils/roles.ts
@@ -11,19 +11,8 @@ export async function getRole(
   name: string
 ): Promise<{ default: boolean; role: Role }> {
   const config = await getConfig();
-  return (
-    {
-      default: name === 'default',
-      role: {
-        ...config.roles[name],
-        // @ts-ignore
-        iconColor: parseInt(config.roles[name].iconColor),
-        // @ts-ignore
-        plusColor: parseInt(config.roles[name].plusColor),
-      },
-    } || {
-      default: true,
-      role: config.roles.default,
-    }
-  );
+
+  if (config.roles.roles[name])
+    return { default: name === 'default', role: config.roles.roles[name] };
+  return { default: true, role: config.roles.default };
 }
