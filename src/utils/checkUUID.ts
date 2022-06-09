432341e56115b7ec85b05876d4a70034375dce42
Richard Dorian <75615715+RichardDorian@users.noreply.github.com>
2022-06-09 07:01:26 +0000

    fix: :bug: fix potential bug with uuid check

--- src/utils/checkUUID.ts
+++ src/utils/checkUUID.ts
@@ -2,7 +2,7 @@ import Player from '../player/Player';
 
 export default function checkUUID(uuid: string, player: Player): boolean {
   const match =
-    /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(uuid);
+    /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(uuid.toLowerCase());
   if (!match) player.sendConsoleMessage(`§c${uuid} is not a valid UUID!`);
   return match;
 }
