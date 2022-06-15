e3f32226f6f4c8a4d5ff24932e29a77692d3d37b
Richard Dorian <75615715+RichardDorian@users.noreply.github.com>
2022-06-15 10:10:42 +0000

    fix: :bug: fix ts errors

--- src/utils/checkUUID.ts
+++ src/utils/checkUUID.ts
@@ -1,6 +1,7 @@
+import ConsolePlayer from '../commands/ConsolePlayer';
 import Player from '../player/Player';
 
-export default function checkUUID(uuid: string, player: Player): boolean {
+export default function checkUUID(uuid: string, player: Player | ConsolePlayer): boolean {
   const match =
     /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(uuid.toLowerCase());
   if (!match) player.sendConsoleMessage(`§c${uuid} is not a valid UUID!`);
