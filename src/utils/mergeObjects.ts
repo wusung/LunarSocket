2fe5bffb08649c5f4ecc700e76b17456dae8b335
Richard Dorian <75615715+RichardDorian@users.noreply.github.com>
2022-06-26 14:56:13 +0000

    fix: :bug: fix config merging

--- src/utils/mergeObjects.ts
+++ src/utils/mergeObjects.ts
@@ -2,7 +2,13 @@ export default function mergeObjects<T = Object>(obj1: T, obj2: T): T {
   const keys = [...new Set([...Object.keys(obj1), ...Object.keys(obj2)])];
 
   for (const key of keys) {
-    if (typeof obj1[key] !== 'object' || typeof obj2[key] !== 'object') continue;
+    if (typeof obj1[key] !== 'object' || typeof obj2[key] !== 'object')
+      continue;
+
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      obj2[key] = [...obj1[key], ...obj2[key]];
+      continue;
+    }
 
     obj2[key] = mergeObjects(obj1[key], obj2[key]);
   }
